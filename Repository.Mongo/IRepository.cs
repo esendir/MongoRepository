93075cb5c84b113b0eb27169de34acfce9ba4edb
Usame Esendir <esendir@hotmail.com>
2017-04-01 00:28:13 +0100

    update async

--- Repository.Mongo/IRepository.cs
+++ Repository.Mongo/IRepository.cs
@@ -350,6 +350,51 @@ namespace Repository.Mongo
         /// <returns>true if successful, otherwise false</returns>
         bool Update<TField>(FilterDefinition<T> filter, Expression<Func<T, TField>> field, TField value);
 
+        /// <summary>
+        /// update a property field in an entity
+        /// </summary>
+        /// <typeparam name="TField">field type</typeparam>
+        /// <param name="entity">entity</param>
+        /// <param name="field">field</param>
+        /// <param name="value">new value</param>
+        Task<UpdateResult> UpdateAsync<TField>(T entity, Expression<Func<T, TField>> field, TField value);
+
+        /// <summary>
+        /// update an entity with updated fields
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <param name="updates">updated field(s)</param>
+        Task<UpdateResult> UpdateAsync(string id, params UpdateDefinition<T>[] updates);
+
+        /// <summary>
+        /// update an entity with updated fields
+        /// </summary>
+        /// <param name="entity">entity</param>
+        /// <param name="updates">updated field(s)</param>
+        Task<UpdateResult> UpdateAsync(T entity, params UpdateDefinition<T>[] updates);
+
+        /// <summary>
+        /// update a property field in entities
+        /// </summary>
+        /// <typeparam name="TField">field type</typeparam>
+        /// <param name="filter">filter</param>
+        /// <param name="field">field</param>
+        /// <param name="value">new value</param>
+        Task<UpdateResult> UpdateAsync<TField>(FilterDefinition<T> filter, Expression<Func<T, TField>> field, TField value);
+
+        /// <summary>
+        /// update found entities by filter with updated fields
+        /// </summary>
+        /// <param name="filter">collection filter</param>
+        /// <param name="updates">updated field(s)</param>
+        Task<UpdateResult> UpdateAsync(FilterDefinition<T> filter, params UpdateDefinition<T>[] updates);
+
+        /// <summary>
+        /// update found entities by filter with updated fields
+        /// </summary>
+        /// <param name="filter">collection filter</param>
+        /// <param name="updates">updated field(s)</param>
+        Task<UpdateResult> UpdateAsync(Expression<Func<T, bool>> filter, params UpdateDefinition<T>[] updates);
         #endregion Update
 
         #endregion CRUD
--- Repository.Mongo/Repository.cs
+++ Repository.Mongo/Repository.cs
@@ -528,6 +528,18 @@ namespace Repository.Mongo
         }
 
         /// <summary>
+        /// update a property field in an entity
+        /// </summary>
+        /// <typeparam name="TField">field type</typeparam>
+        /// <param name="entity">entity</param>
+        /// <param name="field">field</param>
+        /// <param name="value">new value</param>
+        public Task<UpdateResult> UpdateAsync<TField>(T entity, Expression<Func<T, TField>> field, TField value)
+        {
+            return UpdateAsync(entity, Updater.Set(field, value));
+        }
+
+        /// <summary>
         /// update an entity with updated fields
         /// </summary>
         /// <param name="id">id</param>
@@ -536,6 +548,16 @@ namespace Repository.Mongo
         public virtual bool Update(string id, params UpdateDefinition<T>[] updates)
         {
             return Update(Filter.Eq(i => i.Id, id), updates);
+        }
+
+        /// <summary>
+        /// update an entity with updated fields
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <param name="updates">updated field(s)</param>
+        public virtual Task<UpdateResult> UpdateAsync(string id, params UpdateDefinition<T>[] updates)
+        {
+            return UpdateAsync(Filter.Eq(i => i.Id, id), updates);
         }
 
         /// <summary>
@@ -550,6 +572,16 @@ namespace Repository.Mongo
         }
 
         /// <summary>
+        /// update an entity with updated fields
+        /// </summary>
+        /// <param name="entity">entity</param>
+        /// <param name="updates">updated field(s)</param>
+        public virtual Task<UpdateResult> UpdateAsync(T entity, params UpdateDefinition<T>[] updates)
+        {
+            return UpdateAsync(entity.Id, updates);
+        }
+
+        /// <summary>
         /// update a property field in entities
         /// </summary>
         /// <typeparam name="TField">field type</typeparam>
@@ -560,6 +592,18 @@ namespace Repository.Mongo
         public bool Update<TField>(FilterDefinition<T> filter, Expression<Func<T, TField>> field, TField value)
         {
             return Update(filter, Updater.Set(field, value));
+        }
+
+        /// <summary>
+        /// update a property field in entities
+        /// </summary>
+        /// <typeparam name="TField">field type</typeparam>
+        /// <param name="filter">filter</param>
+        /// <param name="field">field</param>
+        /// <param name="value">new value</param>
+        public Task<UpdateResult> UpdateAsync<TField>(FilterDefinition<T> filter, Expression<Func<T, TField>> field, TField value)
+        {
+            return UpdateAsync(filter, Updater.Set(field, value));
         }
 
         /// <summary>
@@ -582,6 +626,20 @@ namespace Repository.Mongo
         /// </summary>
         /// <param name="filter">collection filter</param>
         /// <param name="updates">updated field(s)</param>
+        public Task<UpdateResult> UpdateAsync(FilterDefinition<T> filter, params UpdateDefinition<T>[] updates)
+        {
+            return Retry(() =>
+            {
+                var update = Updater.Combine(updates).CurrentDate(i => i.ModifiedOn);
+                return Collection.UpdateManyAsync(filter, update.CurrentDate(i => i.ModifiedOn));
+            });
+        }
+
+        /// <summary>
+        /// update found entities by filter with updated fields
+        /// </summary>
+        /// <param name="filter">collection filter</param>
+        /// <param name="updates">updated field(s)</param>
         /// <returns>true if successful, otherwise false</returns>
         public bool Update(Expression<Func<T, bool>> filter, params UpdateDefinition<T>[] updates)
         {
@@ -589,6 +647,20 @@ namespace Repository.Mongo
             {
                 var update = Updater.Combine(updates).CurrentDate(i => i.ModifiedOn);
                 return Collection.UpdateMany(filter, update).IsAcknowledged;
+            });
+        }
+
+        /// <summary>
+        /// update found entities by filter with updated fields
+        /// </summary>
+        /// <param name="filter">collection filter</param>
+        /// <param name="updates">updated field(s)</param>
+        public Task<UpdateResult> UpdateAsync(Expression<Func<T, bool>> filter, params UpdateDefinition<T>[] updates)
+        {
+            return Retry(() =>
+            {
+                var update = Updater.Combine(updates).CurrentDate(i => i.ModifiedOn);
+                return Collection.UpdateManyAsync(filter, update);
             });
         }
 
