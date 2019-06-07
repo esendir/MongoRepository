d582476eeb9a45a1378b8b835c1268143405d9f9
Usame Esendir <esendir@hotmail.com>
2019-06-07 23:48:32 +0100

    performance update

---
+++ Repository.Mongo.Tests/ConstructorTests.cs
@@ -0,0 +1,67 @@
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System.Diagnostics;
+using Xunit;
+
+namespace Repository.Mongo.Tests
+{
+    public class ConstructorTests
+    {
+        [Fact]
+        public void Constructor_IMongoDatabase_ShouldBe_LessThen5ms()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var param1 = fixture.Create<IMongoDatabase>();
+            var param2 = fixture.Create<string>();
+
+            var watch = Stopwatch.StartNew();
+
+            new Repository<IEntity>(param1, param2);
+
+            var duration = watch.ElapsedMilliseconds;
+            Assert.True(duration < 5);
+        }
+
+        [Fact]
+        public void Constructor_IMongoClient_ShouldBe_LessThen5ms()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var param1 = fixture.Create<IMongoClient>();
+            var param2 = fixture.Create<string>();
+            var watch = Stopwatch.StartNew();
+
+            new Repository<IEntity>(param1, param2, param2);
+
+            var duration = watch.ElapsedMilliseconds;
+            Assert.True(duration < 5);
+        }
+
+        [Fact]
+        public void Constructor_IConfiguration_ShouldBe_LessThen5ms()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var param1 = fixture.Create<IConfiguration>();
+            var watch = Stopwatch.StartNew();
+
+            new Repository<IEntity>(param1);
+
+            var duration = watch.ElapsedMilliseconds;
+            Assert.True(duration < 5);
+        }
+
+        [Fact]
+        public void Constructor_ConnectionString_ShouldBe_LessThen5ms()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var param1 = fixture.Create<string>();
+            var watch = Stopwatch.StartNew();
+
+            new Repository<IEntity>(param1, param1);
+
+            var duration = watch.ElapsedMilliseconds;
+            Assert.True(duration < 5);
+        }
+    }
+}
